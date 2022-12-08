22a7c26cd4c025a504a858900f92708d32bfc36b
e.pustovaya <epustovaya@quintegro.com>
2022-12-08 22:28:09 +0100

    add ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -5,8 +5,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
-}
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
